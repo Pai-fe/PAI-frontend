0e0895e4ce37c6a5aa9d9d672bfd026300b2dff8
kdokic1 <kdokic1@etf.unsa.ba>
2021-05-25 20:31:22 +0200

    navbar and login design added

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,6 +2,11 @@ import React, {useState} from 'react';
 import {Link} from "react-router-dom";
 import * as FaIcons from "react-icons/fa"
 import * as AiIcons from "react-icons/ai"
+import logo from './images/logo.png'
+import homeImg from './images/home.png'
+import userImg from './images/user.png'
+
+
 import './NavBar.css'
 
 function NavBar() {
@@ -15,11 +20,13 @@ function NavBar() {
         {
             title: 'Home',
             path: '/',
+            image: homeImg,
             cName: 'nav-text'
         },
         {
             title: 'Users',
             path: '/users',
+            image: userImg,
             cName: 'nav-text'
         }
     ]
@@ -30,6 +37,9 @@ function NavBar() {
                 <Link to="#" className='menu-bars'>
                     <FaIcons.FaBars onClick={showSidebar}/>
                 </Link>
+            </div>
+            <div className='logo'>
+                <img src={logo}/>
             </div>
             <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
                 <ul className='nav-menu-items' onClick={showSidebar}>
@@ -42,13 +52,21 @@ function NavBar() {
                         return (
                             <li key={index} className={item.cName}>
                                 <Link to={item.path}>
+                                    <div className='meni-icons'>
+                                        <img  src={item.image}/>
+                                    </div>
                                     <span className='item-span'>{item.title}</span>
                                 </Link>
                             </li>
+                            
                         )
                     })}
                 </ul>
             </nav>
+
+            
+            
+            
         </div>
     )
 }
