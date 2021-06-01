3a8b2073c1d73d6229029b6c332be0a44f0d6ba9
mehmed <mehmeddziho98@gmail.com>
2021-06-01 12:45:14 +0200

    report wip

--- src/components/Pages/Campaigns.jsx
+++ src/components/Pages/Campaigns.jsx
@@ -18,118 +18,145 @@ import { routes } from '../AppRouter/RoutesList';
 import './Table.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { FaTrash, FaEdit, FaPlus } from 'react-icons/fa';
+import { FaTrash, FaEdit, FaPlus, FaFileExport } from 'react-icons/fa';
 
 import swal from "sweetalert";
 
 
 
 const Campaigns = () => {
-    const [campaigns, setCampaigns]  = React.useState([]);
+    const [campaigns, setCampaigns] = React.useState([]);
     const dispatch = useDispatch();
     const history = useHistory();
     const currentUser = useSelector(state => state?.userInfo?.user);
     const camps = useSelector(state => state?.camps?.list);
 
 
-    const onLoad = async() => {
-        try{
-            const { data, status } = await axiosHelperCall('GET', `https://si-projekat2.herokuapp.com/api/campaign/all`, {}, {});
-            if(status !== 200) throw new Error();
-            
+    const onLoad = async () => {
+        try {
+            const {
+                data,
+                status
+            } = await axiosHelperCall('GET', `https://si-projekat2.herokuapp.com/api/campaign/all`, {}, {});
+            if (status !== 200) throw new Error();
+
             console.log("data", data);
             setCampaigns(data);
-            
-        }catch(e){
-            console.log('TAG-ERROR','FAILED REQUEST AT FAs.jsx');
+
+        } catch (e) {
+            console.log('TAG-ERROR', 'FAILED REQUEST AT FAs.jsx');
         }
-      }
+    }
 
-      // const onLoad2 = async()=>{
-      //   try{
-      //     const { data, status } = await axiosHelperCall('GET', `${CONFIG.APP_URL}/faDefinition/all`, {}, {});
-      //     if(status !== 200) throw new Error();
-      //     console.log("data", data);
-      //     //setCampaigns(data);
-          
-      // }catch(e){
-      //     console.log('TAG-ERROR','FAILED REQUEST AT FAs.jsx');
-      // }
-      // }
+    // const onLoad2 = async()=>{
+    //   try{
+    //     const { data, status } = await axiosHelperCall('GET', `${CONFIG.APP_URL}/faDefinition/all`, {}, {});
+    //     if(status !== 200) throw new Error();
+    //     console.log("data", data);
+    //     //setCampaigns(data);
 
-      useEffect(()=>{
+    // }catch(e){
+    //     console.log('TAG-ERROR','FAILED REQUEST AT FAs.jsx');
+    // }
+    // }
+
+    useEffect(() => {
         onLoad();
         //onLoad2();
-      }, []);
+    }, []);
 
-      const onEditCampaign = async(camp) => {
+    const onExportCampaign = (camp) => {
+        history.push(routes.CreateCampaignReport.replace(':id', camp.CamapignId))
+    }
+
+    const onEditCampaign = async (camp) => {
         //dispatch(setFA(fa));
         console.log(camp)
-        setTimeout(()=>{
-          history.push(routes.CAMP_EDIT_VIEW.replace(':id', camp.CamapignId))
+        setTimeout(() => {
+            history.push(routes.CAMP_EDIT_VIEW.replace(':id', camp.CamapignId))
         }, 1000)
-      }
+    }
 
-      const onDeleteCamp = async (camp)=>{
-        try{
-          const { data, status } = await axiosHelperCall('DELETE', `https://si-projekat2.herokuapp.com/api/campaign/delete/`+camp.CamapignId, {}, {});
-          if(status === 200) {
-            swal({
-              title: "Success!",
-              text: "You have successfully deleted a campaign.",
-              icon: "success"
-            }).then(value => {
-              onLoad();
-            });
-          }
-      }catch(e){
-          console.log('TAG-ERROR','FAILED REQUEST AT FAs.jsx');
-      }
-      }
+    const onDeleteCamp = async (camp) => {
+        try {
+            const {
+                data,
+                status
+            } = await axiosHelperCall('DELETE', `https://si-projekat2.herokuapp.com/api/campaign/delete/` + camp.CamapignId, {}, {});
+            if (status === 200) {
+                swal({
+                    title: "Success!",
+                    text: "You have successfully deleted a campaign.",
+                    icon: "success"
+                }).then(value => {
+                    onLoad();
+                });
+            }
+        } catch (e) {
+            console.log('TAG-ERROR', 'FAILED REQUEST AT FAs.jsx');
+        }
+    }
 
-      return ( 
+    return (
         <div className='fa-list-view'>
-          <table className="table">
-            <thead className="thead-dark">
+            <table className="table">
+                <thead className="thead-dark">
                 <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Start date</th>
-                <th>End date</th>
-                {currentUser?.uloga === "Admin" && (<th>Edit</th>)}
-                {currentUser?.uloga === "Admin" && (<th>Delete</th>)}
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Start date</th>
+                    <th>End date</th>
+                    <th>Report</th>
+                    {currentUser?.uloga === "Admin" && (<th>Edit</th>)}
+                    {currentUser?.uloga === "Admin" && (<th>Delete</th>)}
                 </tr>
-            </thead>
-            <tbody>
+                </thead>
+                <tbody>
                 {campaigns.map(camp => (
                     <tr key={camp?.CamapignId}>
                         <td>{camp?.CamapignId}</td>
                         <td>{camp?.Name}</td>
                         <td>{camp?.StartDate.substring(0,10)}</td>
                         <td>{camp?.EndDate.substring(0,10)}</td>
-                        {currentUser?.uloga === "Admin" && (<td>
-                            <button 
+                        <td>{camp?.StartDate}</td>
+                        <td>{camp?.EndDate}</td>
+                        <td>
+                            <button
                                 className='btn btn-link'
-                                onClick={() => onEditCampaign(camp)}
-                            >
-                        <FaEdit/></button></td>)}
-                            {currentUser?.uloga === "Admin" && (<td><button 
-                            className='btn btn-link'
-                            onClick={() => onDeleteCamp(camp)}
-                        >
-                        <FaTrash/></button></td>)}
+                                onClick={() => onExportCampaign(camp)}>
+                                <FaFileExport/>
+                            </button>
+                        </td>
+                        {currentUser?.uloga === "Admin" && (<td>
+                                <button
+                                    className='btn btn-link'
+                                    onClick={() => onEditCampaign(camp)}>
+                                    <FaEdit/>
+                                </button>
+                            </td>
+                        )}
+                        {currentUser?.uloga === "Admin" && (<td>
+                            <button
+                                className='btn btn-link'
+                                onClick={() => onDeleteCamp(camp)}>
+                                <FaTrash/>
+                            </button>
+                        </td>)}
                     </tr>
                 ))}
-              </tbody>
-          </table>
-          {currentUser?.uloga === "Admin" && 
-          (<button 
-              className='btn btn-success'
-              onClick={() => {console.log("Tusam");history.push(routes.CampaignCreateView)}}
+                </tbody>
+            </table>
+            {currentUser?.uloga === "Admin" &&
+            (<button
+                className='btn btn-success'
+                onClick={() => {
+                    console.log("Tusam");
+                    history.push(routes.CampaignCreateView)
+                }}
             >
-              <FaPlus/>CREATE NEW CAMPAIGN</button>)}
+                <FaPlus/>CREATE NEW CAMPAIGN</button>)}
         </div>
-      );
+    );
 }
 
 export default Campaigns;
